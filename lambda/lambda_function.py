18b1ae7bc3f6961eebf1cb01fbeb53e7ea1205f8
Alexander Vanadio <avanadio@gmail.com>
2024-12-12 11:21:02 -0500

    fixed bugs

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -176,6 +176,7 @@ sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(HelloWorldIntentHandler())
+sb.add_request_handler(ZigZagIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
